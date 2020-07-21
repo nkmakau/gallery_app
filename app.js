f75fb32266ca66dc51921e06c2a6f27d44f4227b
nkmakau <noshmak@gmail.com>
2020-07-21 15:58:29 +0300

    add moongoose and connect db

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 // Import express package
 const express = require('express');
+const mongoose = require('mongoose');
 
 let indexRouter = require('./routes/index');
+
+//Connecting to the Database
+let mongodb_url = 'mongodb://localhost/';
+let dbName = 'darkroom';
+mongoose.connect(mongodb_url + dbName,)
+let db = mongoose.connection;
+
+//Check Connection
+db.once('open', () => {
+    console.log('Database connected Successfully')
+})
+
+//Check for DB Errors
+db.on('error', (error) => {
+    console.log(error);
+})
 
 // Initialize express
 const app = express();
