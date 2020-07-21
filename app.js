174cd5241237e47a9f23be120c8241364edadfc2
nkmakau <noshmak@gmail.com>
2020-07-21 18:00:18 +0300

    add view files and upload engine

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const express = require('express');
 // Initialize express
 const app = express();
 
+// Set up a view engine
+app.set('view engine', 'ejs');
+
+
 // Set a static folder
 app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.render('index');
+ })
 
 // Define the port number
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
-   console.log(`Server is listening on port ${PORT}`)
-})
+    console.log(`Server is listening on port ${PORT}`)
+});
