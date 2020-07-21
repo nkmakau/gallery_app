0b985b4d0e1e70337da50f2d99b399158ae0fe1b
nkmakau <noshmak@gmail.com>
2020-07-21 17:53:27 +0300

    add public folder

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// Import express framework
+// Import express package
 const express = require('express');
+
 
 // Initialize express
 const app = express();
 
+// Set a static folder
+app.use(express.static('public'));
+
 // Define the port number
 const PORT = 5000;
 
-
-app.listen(PORT, function(){
+app.listen(PORT, ()=>{
    console.log(`Server is listening on port ${PORT}`)
 })
