6d4d287ecf76bd9d070e3232be013cc327950265
nkmakau <noshmak@gmail.com>
2020-07-22 11:39:00 +0300

    Corrected image display

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,44 @@ const router = express.Router();
 const upload = require('../upload');
 const Photo = require('../models/photos');
 
-router.get('/', (req, res) => {
-    res.render('index');
-})
+
+// Home page router
+router.get('/', (req,res)=>{
+    Photo.find({}, (error, images)=>{
+        if (error){
+            console.log(error);
+        }else{
+ 
+            res.render('index', { images:images, msg: req.query.msg});
+        }
+    })
+ })
 
 // route to handle image upload
-router.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
+router.post('/upload', (req,res)=>{
+    upload(req, res, (err)=>{
+        if (err){
             console.log(err)
             // Redirect to the index page and pass a msg to the template
             res.redirect(`/?msg=${err}`);
-        } else if (req.file === undefined) {
+        }else if(req.file === undefined){
             // Redirect to the index page and pass a msg to the template
             res.redirect('/?msg=Error: No file selected!');
-        } else {
+        }else{
             // Create a new photo
             let newPhoto = new Photo({
                 name: req.file.filename,
                 path: 'images/' + req.file.filename,
                 size: req.file.size
             })
-
+ 
             // Save newPhoto in the database
             newPhoto.save()
-
+ 
             // Redirect to the index page and pass a msg to the template
             res.redirect('/?msg=File uploaded successfully');
         }
     })
-})
+ })
 
 module.exports = router;
